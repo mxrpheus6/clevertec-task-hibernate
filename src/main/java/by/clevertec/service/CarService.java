a0b6627b41832508429c336b4d1a04d8addcdbd2
Pavel Kazachenko <pkazachenkoo@gmail.com>
2024-11-29 01:39:36 +0300

    added car search with filters

--- src/main/java/by/clevertec/service/CarService.java
+++ src/main/java/by/clevertec/service/CarService.java
@@ -3,10 +3,12 @@ package by.clevertec.service;
 import by.clevertec.entity.Car;
 import by.clevertec.entity.CarShowroom;
 import by.clevertec.util.HibernateUtil;
+import jakarta.persistence.criteria.Predicate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -93,6 +95,51 @@ public class CarService {
                 throw e;
             }
             return car;
+        }
+    }
+
+    List<Car> findCarsByFilters(String brand,
+                                String category,
+                                Integer manufactureYear, Double minPrice,
+                                Double maxPrice) {
+        try (Session session = hibernateUtil.getSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                var criteriaBuilder = session.getCriteriaBuilder();
+                var criteriaQuery = criteriaBuilder.createQuery(Car.class);
+                var carRoot = criteriaQuery.from(Car.class);
+
+                List<Predicate> predicates = new ArrayList<>();
+
+                if (brand != null && !brand.isEmpty()) {
+                    predicates.add(criteriaBuilder.equal(carRoot.get("brand"), brand));
+                }
+
+                if (category != null && !category.isEmpty()) {
+                    predicates.add(criteriaBuilder.equal(carRoot.get("category"), category));
+                }
+
+                if (manufactureYear != null) {
+                    predicates.add(criteriaBuilder.equal(carRoot.get("manufactureYear"), manufactureYear));
+                }
+
+                if (minPrice != null) {
+                    predicates.add(criteriaBuilder.greaterThanOrEqualTo(carRoot.get("price"), minPrice));
+                }
+
+                if (maxPrice != null) {
+                    predicates.add(criteriaBuilder.lessThanOrEqualTo(carRoot.get("price"), maxPrice));
+                }
+
+                criteriaQuery.select(carRoot).where(criteriaBuilder.and(predicates.toArray(new Predicate[0])));
+                List<Car> resultList = session.createQuery(criteriaQuery).getResultList();
+
+                transaction.commit();
+                return resultList;
+            } catch (Exception e) {
+                transaction.rollback();
+                throw e;
+            }
         }
     }
 }
